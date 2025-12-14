5d8093958622d4a990e87adadbe0671294f0ce9a
jayeshbhere <jkbhere09@gmail.com>
2025-12-14 19:36:41 +0530

    Project Started

---
+++ src/test/java/com/eventflow/eventManagement/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventflow.eventManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
